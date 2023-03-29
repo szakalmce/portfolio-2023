538616e41345de6bbbeb56d3296274762459d919
TomaszGarbarz <tomasz.garbarz91@gmail.com>
2023-03-29 20:22:21 +0200

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        url: 'http://portfolio2023.local/graphql',
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
+};
