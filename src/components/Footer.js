9e70d0093e65e9f14c3f0fb3616d4118a82c2ce5
szakalmce <tomasz.garbarz91@gmail.com>
2023-04-08 20:15:08 +0200

    ss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,13 @@ import {
   FooterWrapper,
   FooterSocialLinksWrapper,
   FooterContent,
-} from '../../styles/styles';
-import SocialLinks from '../SocialLinks/SocialLinks';
+} from '../styles/styles';
+import SocialLinks from './SocialLinks/SocialLinks';
 
 const Footer = () => {
   const [value, setValue] = useState(0);
 
   console.log(process.env.REACT_APP_CONTENTFUL_TOKEN);
-  const handleClick = () => {
-    setValue((prevState) => {
-      const state = prevState + 1;
-      console.log(state);
-      return state;
-    });
-  };
 
   return (
     // <FooterWrapper>
